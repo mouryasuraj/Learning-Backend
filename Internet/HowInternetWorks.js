0b90d86c4e1a225e07446d451b1984fe9e6d912c
Suraj <mouryasuraj6266444@gmail.com>
2024-03-18 20:03:16 +0530

    Initial Commit

---
+++ Internet/HowInternetWorks.js
@@ -0,0 +1,28 @@
+/*
+
+// Switch
+1. cat5 and cat6 are the type of cables. cat stand for category
+2. cat 6 cables are faster than cat 5.
+3. Some of the switch device use fibre optic cables which is faster than cat 6.
+4. Switch is used to connect bunch of computers so that they can commnunicate each other.
+5. Switch is used in environment like office,school, colleges.
+6. Switch used cables to connect to computers
+7. If we want to connect our devices to the switch devices then we have to use copper cables or fibre optic, we are not able to connect wirelessly.
+8. Switch have ports to connect to the device, the higher the number of ports the higher is the price
+9. These ports are also known as LAN ports
+
+// Access Point 
+1. If you want to connect devices in the same environment then we have to use access point devices
+ 
+//And this is known as LAN or local area network
+
+//Packets or Frame
+1. Data can divided into small packet which have some infornmation like header, payload and trailer
+
+
+// What is router device?
+1. The main task of router is to enable computers to connect to the internet.
+
+
+// Home router is a combination of switch and router and now a days most of the home router have access point feature.
+*/
